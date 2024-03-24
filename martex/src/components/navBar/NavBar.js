15b73cd7854aba7d180ec71892d248c21776d4b8
ayoub <151740084+Ayoub-benali07@users.noreply.github.com>
2024-03-24 14:36:22 +0100

    navbar.js
    navbar et home

---
+++ martex/src/components/navBar/NavBar.js
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+const NavBar = () => {
+    return (
+        <>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+                
+            </ul>
+        </nav>
+        <Outlet />
+        </>
+    )
+};
+export default NavBar;

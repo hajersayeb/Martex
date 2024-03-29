79497e7ad6084e2bfebf444973c569b6943177e3
hajersayeb <145911124+hajersayeb@users.noreply.github.com>
2024-03-29 14:19:10 +0100

    index
    navbar

---
+++ martex/martex/src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Acceuil from "./pages/Acceuil";
+import About from "./pages/About";
+import Features from "./pages/Features";
+import Login from "./pages/Login";
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route index element={<Acceuil />} />
+        <Route path="about" element={<About />} />
+        <Route path="features" element={<Features />} />
+        <Route path="login" element={<Login />} />
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

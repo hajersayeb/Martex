987db27b6db04ee814069aab316cd904abf8e0bd
hajersayeb <145911124+hajersayeb@users.noreply.github.com>
2024-03-30 11:09:32 +0100

    navbar.js
    bootstrap navbar

--- martex/martex/src/components/navBar/NavBar.js
+++ martex/martex/src/components/navBar/NavBar.js
@@ -1,22 +1,30 @@
 import { Outlet, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 const NavBar = () => {
     return (
         <>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Acceuil</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/features">Features</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
+       
+       <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <Link class="navbar-brand"to="/">Acceuil</Link>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <Link class="nav-link active" aria-current="page" to='/about'>About</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to='/features'>Features</Link>
+        </li>
+        <li class="nav-item">
+          <Link class="nav-link" to='login'>Login</Link>
+        </li>
+      </ul>
+    </div>
+            
+  </div>
         </nav>
         <Outlet />
         </>
--- martex/martex/src/pages/Acceuil.js
+++ martex/martex/src/pages/Acceuil.js
@@ -1,3 +1,7 @@
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+import '../pages/styles/Acceuil.css'
+
 const Acceuil = () => {
     return <h1>Home</h1>;
 };

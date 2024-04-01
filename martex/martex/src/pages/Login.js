6729d706866e4feefac188e8a9b5839600f7923d
hajersayeb <145911124+hajersayeb@users.noreply.github.com>
2024-04-02 00:28:21 +0200

    submitting forms
    function const value onchange

--- martex/martex/src/pages/Login.js
+++ martex/martex/src/pages/Login.js
@@ -3,8 +3,14 @@ import loginImg from "../pages/images/office.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserLock } from '@fortawesome/free-solid-svg-icons';
 import '../pages/styles/Login.css';
-
-const Login = () => {
+import { useState } from 'react';
+function Login() {
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(`The email you entered was: ${email}, The password you entered was: ${password}`);
+}
 
     return(
         <>
@@ -17,7 +23,7 @@ const Login = () => {
 
 </div>
 <div className='col-6 lg-6 sm-3'>
-    <form action="">
+    <form onSubmit={handleSubmit}>
         <h1>
             Login your account
         </h1>
@@ -25,20 +31,23 @@ const Login = () => {
 
     <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
     <div class="col-sm-10">
-      <input type="email" readonly class="form-control" id="s" placeholder="email" />
+      <input type="email" readonly class="form-control" id="s" placeholder="email" value={email} 
+      onChange={(e) => setEmail(e.target.value)} />
     </div>
   </div>
   <div class="mb-3 row">
 
     <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="" placeholder="password" />
+      <input type="password" class="form-control" id="" placeholder="password" value={password}
+      onChange={(p) => setPassword(p.target.value)} />
+
     </div>
   </div>
-  <button className="btn btn-danger">Login</button>
-  <FontAwesomeIcon icon={faUserLock} />
+  <button type="submit" className="btn btn-danger">Login</button>
+  
   <p>
-    create new account
+  <FontAwesomeIcon icon={faUserLock} /> create new account
   </p>
   </form>
 </div>
